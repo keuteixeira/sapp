45ca6e8c40d4f8d66a7a2f084549f855685cf15f
Cleiton Teixeira <cleiton.teixeiras@gmail.com>
2024-05-20 16:35:40 -0300

    Update PDFUtilFnc.php

--- src/PDFUtilFnc.php
+++ src/PDFUtilFnc.php
@@ -482,6 +482,8 @@ class PDFUtilFnc {
         // convert signature to hex
         $signature = current(unpack('H*', $signature));
         $signature = str_pad($signature, __SIGNATURE_MAX_LENGTH, '0');       
+		
+		@unlink($temp_filename);
 
         return $signature;
     }   
